690d8ad3f10c799cfa6ec156a18752176cd1399a
Thomas Templeton <thomas.templeton.dev@gmail.com>
2020-01-21 00:49:56 +1100

    Add Space tests

--- src/space.rs
+++ src/space.rs
@@ -47,3 +47,61 @@ pub enum SpaceState {
     Unchecked,
     Checked(bool),
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn set_checked() {
+        let mut space = Space::new([0, 0]);
+        assert!(space.set_checked(false).is_ok());
+        assert!(space.set_checked(false).is_err());
+
+        space = Space::new([0, 0]);
+        assert!(space.set_checked(true).is_ok());
+        assert!(space.set_checked(true).is_err());
+    }
+
+    #[test]
+    fn is_unchecked() {
+        let mut space = Space::new([0, 0]);
+        assert!(space.is_unchecked());
+        assert!(space.set_checked(false).is_ok());
+        assert!(!space.is_unchecked());
+
+        space = Space::new([0, 0]);
+        assert!(space.set_checked(true).is_ok());
+        assert!(!space.is_unchecked());
+    }
+
+    #[test]
+    fn is_empty() {
+        let mut space = Space::new([0, 0]);
+        assert!(!space.is_empty());
+        assert!(space.set_checked(false).is_ok());
+        assert!(space.is_empty());
+
+        space = Space::new([0, 0]);
+        assert!(space.set_checked(true).is_ok());
+        assert!(!space.is_empty());
+    }
+
+    #[test]
+    fn is_hit() {
+        let mut space = Space::new([0, 0]);
+        assert!(!space.is_hit());
+        assert!(space.set_checked(true).is_ok());
+        assert!(space.is_hit());
+
+        space = Space::new([0, 0]);
+        assert!(space.set_checked(false).is_ok());
+        assert!(!space.is_hit());
+    }
+
+    #[test]
+    fn pos() {
+        let space = Space::new([0, 0]);
+        assert_eq!(space.pos(), &[0, 0]);
+    }
+}
